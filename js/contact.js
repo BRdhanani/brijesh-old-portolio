9f57b2f127380ab32842564ca4f52a6d2741ecd8
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-04-19 12:08:23 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,32 +1,85 @@
-function sendEmail(e) {
-  e.preventDefault();
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
 
-  const POST_URL = 'https://script.google.com/macros/s/AKfycbyOdXmjKDMGNgOd9oSZ-sL9Sb8nN028MrA5Pw7ysYEUefuHqyk/exec';
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
 
-  const postRequest = {
-    name: e.target['name'].value,
-    email: e.target['email'].value,
-    subject: e.target['subject'].value,
-    body: e.target['message'].value
-  };
-  
-  if(POST_URL) {
-    $.post(POST_URL, JSON.stringify(postRequest))
-      .then(res => {
-      
-        e.target.reset();
-        $('#alert-field')
-          .removeClass()
-          .addClass(`alert alert-${res.code}`)
-          .text(res.msg);
-      });
-
-    $('#alert-field')
-      .removeClass()
-      .html('<progress></progress>')
-      .removeClass('hidden');
-  } else {
-    alert('You must set the POST_URL variable with your script ID');
-  }
-
-}
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "come on, you have a name, don't you?",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                subject: {
+                    required: "come on, you have a subject, don't you?",
+                    minlength: "your subject must consist of at least 4 characters"
+                },
+                number: {
+                    required: "come on, you have a number, don't you?",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "no email, no message"
+                },
+                message: {
+                    required: "um...yea, you have to write something to send this form.",
+                    minlength: "thats all? really?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
