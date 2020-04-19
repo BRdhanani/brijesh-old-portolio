0a81384c98b44da50baed8b226113fcfbf1603a4
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-04-19 11:49:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -386,39 +386,6 @@ $(document).ready(function() {
   wow.init();
 });
 
-
-function sendEmail(e) {
-  e.preventDefault();
-
-  const POST_URL = 'https://script.google.com/macros/s/AKfycbyOdXmjKDMGNgOd9oSZ-sL9Sb8nN028MrA5Pw7ysYEUefuHqyk/exec';
-
-  const postRequest = {
-    name: e.target['name'].value,
-    email: e.target['email'].value,
-    subject: e.target['subject'].value,
-    body: e.target['message'].value
-  };
-  
-  if(POST_URL) {
-    $.post(POST_URL, JSON.stringify(postRequest))
-      .then(res => {
-      
-        e.target.reset();
-        $('#alert-field')
-          .removeClass()
-          .addClass(`alert alert-${res.code}`)
-          .text(res.msg);
-      });
-
-    $('#alert-field')
-      .removeClass()
-      .html('<progress></progress>')
-      .removeClass('hidden');
-  } else {
-    alert('You must set the POST_URL variable with your script ID');
-  }
-
-}
 	
 //------- Mailchimp js --------//  
 function mailChimp() {
